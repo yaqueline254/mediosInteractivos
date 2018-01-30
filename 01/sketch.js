54229afb173566293edd476e026e7322b1ccf6bd
yaqueline254 <35973482+yaqueline254@users.noreply.github.com>
2018-01-30 14:46:31 -0500

    Add files via upload

---
+++ 01/sketch.js
@@ -0,0 +1,122 @@
+function setup() { 
+
+  createCanvas(800, 350); //Tamaño Papel
+
+  background(238,228,194); //Color Papel
+
+strokeWeight(2);
+line(760,0,760,240); //Dibujar Línea
+line(770,0,770,240);//Dibujar Línea
+line(730,24,800,24);//Dibujar Línea
+line(730,27,800,27);//Dibujar Línea
+line(735,200,800,200);//Dibujar Línea
+line(735,203,800,203);//Dibujar Linea
+line(735,206,800,206);//Dibujar Línea
+
+stroke(0,0,0)//Color de la Línea
+strokeWeight(3); // Tamaño pincel
+ellipse(710,170,62,62);//Dibujar Círculo
+
+stroke(0,0,0);//Color de la Línea
+strokeWeight(1); // Tamaño pincel
+ellipse(710,170,55,55)
+
+stroke(0,0,0); // Color de la linea
+strokeWeight(1); // Tamaño pincel
+ellipse(710,170,50,50)//Dibujar Círculo
+
+fill(0,0,255);//Color de relleno
+ellipse(710,170,45,45);//Dibujar Círculo
+
+fill(255,0,0);//Color Relleno
+ellipse(380,260,30,30);//Dibujar Círculo
+
+fill(255,255,255);//Color Relleno
+ellipse(375,380,130,130);//Dibujar Círculo
+
+stroke(0,0,0);//Color Relleno 
+strokeWeight(6);//Tamaño Píncel 
+ellipse(375,380,85,85)
+fill(255,0,0);//Color Relleno
+ellipse(375,380,80,80);//Dibujar Círculo
+strokeWeight(1);//Tamaño Píncel
+stroke(255,255,255);//Color Línea
+fill(130,130,130);//Color del relleno
+ellipse(460,75,50,50);//Dibujar Círculo
+strokeWeight(1); // Tamaño pincel
+  noStroke(); // Color de la linea
+  fill(243,175,188); // Color del relleno
+  rect(770,270,800,350); // Dibujar rectángulo
+fill(0,0,0);
+line(390,98,730,310);//Dibujar Línea
+  line(400,170,730,310);//Dibujar Línea
+beginShape();
+fill(249,219,198); // Color del relleno
+vertex(475,360);// Dibujar Vértice
+vertex(675,360);// Dibujar Vértice
+vertex(575,0);// Dibujar Vértice
+endShape();
+beginShape();
+noStroke()
+fill(255,215,0); // Color del relleno
+vertex(590,150);// Dibujar Vértice
+vertex(670,150);// Dibujar Vértice
+vertex(630,75);// Dibujar Vértice
+endShape();
+strokeWeight(3);//Tamaño Píncel
+stroke(0,0,0);//Color Línea
+line(574,4,476,350);//
+strokeWeight(3);//Tamaño Píncel
+stroke(0,0,0);//Color Línea
+line(575,4,673,350);//Dibujar Línea
+beginShape();
+noStroke()
+fill(135,206,255); // Color del relleno
+vertex(30,350);//Dibujar Vértice
+vertex(71,320);//Dibujar Vértice
+vertex(110,350);//Dibujar Vértice
+endShape();
+strokeWeight(2);//Tamaño Píncel
+stroke(0,0,0);//Tamaño Línea
+line(30,350,71,320);//Dibujar Línea
+strokeWeight(2);//Tamaño Píncel
+stroke(0,0,0);//Tamaño Línea
+line(110,350,71,320);//Dibujar Línea
+line(70,350,70,100); // Dibujar Linea
+line(0,150,260,215); // Dibujar linea
+line(10,260,130,260);//Dibuja Linea
+line(20,290,140,290);//Dibujar Línea
+line(390,98,730,310);//Dibujar Línea
+line(400,170,730,310);//Dibujar Línea
+line(760,350,800,320);//Dibujar Línea
+line(780,350,800,335);//Dibujar Línea
+noStroke();//Color Línea
+fill(173,223,173);//Color del relleno
+ellipse(576,310,60,60);//Dibujar Círculo
+strokeWeight(1);//Tamaño Píncel
+stroke(0,0,0);//Color Línea
+fill(255,255,255);//Color del relleno
+ellipse(576,310,40,40);//Dibujar Círculo
+strokeWeight(1);//Tamaño Píncel
+point(570,325);//Dibujar Punto
+point(576,295);//Dibujar Punto
+point(580,295);//Dibujar Punto
+point(582,290);//Dibujar Punto
+point(569,325);//Dibujar Punto
+point(570,308);//Dibujar Punto
+point(572,310);//Dibujar Punto
+point(571,302);//Dibujar Punto
+point(578,312);//Dibujar Punto
+point(568,315);//Dibujar Punto
+point(572,320);//Dibujar Punto
+point(571,325);//Dibujar Punto
+point(583,321);//Dibujar Punto
+point(582,315);//Dibujar Punto
+point(560,325);//Dibujar Punto
+point(590,320);//Dibujar Punto
+point(590,319);//Dibujar Punto
+point(580,320);//Dibujar Punto
+point(570,325);//Dibujar Punto
+point(280,325);//Dibujar Punto
+	
+}
